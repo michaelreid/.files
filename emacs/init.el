b37734cfea173886453bfe66d70aa3c03ca57003
michael reid <michaelreid@users.noreply.github.com>
2015-05-03 08:32:56 +0930

    Separating bash file into bash_profile and bash_aliases

--- emacs/init.el
+++ emacs/init.el
@@ -67,3 +67,15 @@
 
 ;; All yes or no queries to 'y or n'
 (fset 'yes-or-no-p 'y-or-n-p)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
